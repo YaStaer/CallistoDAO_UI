b69371c7d40201fc1222508e8b710bae75c445b7
YaStaer_work <yastaer@yandex.ru>
2024-04-04 07:26:58 +0300

    fix parsing links

--- utils/interact.js
+++ utils/interact.js
@@ -105,17 +105,22 @@ export const parseSource = data => {
 }
 
 export const parseComment = comment => {
+  // const comment = 'https://onboard.blocknative.com/docs/modules/core?'
   const temp = comment.split(' ')
   const arr = []
   for (const i = 0; i < temp.length; i++) {
     if (temp[i].search('://') != -1) {
-      arr[i] = '<a class="underline" target="_blank" href="' + temp[i] + '">' + temp[i] + '</a>'
+      const nodots = temp[i].replace(/[\.\,\!\?]$/, '')
+      arr[i] = '<a class="underline" target="_blank" href="' + nodots + '">' + nodots + '</a>'
+      if (nodots != temp[i]) {
+        arr[i] = arr[i] + temp[i][temp[i].length - 1]
+      }
     } else {
       arr[i] = temp[i]
     }
   }
   // console.log(temp)
-  // console.log(arr)
+  console.log(arr)
   return 'Comment\u00A0-\u00A0' + arr.join('\u00A0')
 }
 
